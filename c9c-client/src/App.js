4118298a7f5250fb915b4d37637b41f255e02e6a
erincronie <erincronie@gmail.com>
2024-05-29 14:31:02 -0700

    added About page optimized for SEO

--- c9c-client/src/App.js
+++ c9c-client/src/App.js
@@ -1,10 +1,12 @@
-import { Helmet } from 'react-helmet';
+// import { Helmet } from 'react-helmet';
 import Header from './components/Header/Header';
+import About from './components/About/About';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <About />
   
     </div>
   );
