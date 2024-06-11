c0572c55cc603cef290095b71d01891fd35f8c42
erincronie <erincronie@gmail.com>
2024-06-11 16:20:03 -0700

    Fix build directory and server static files configuration

--- c9c-server/src/server.js
+++ c9c-server/src/server.js
@@ -3,13 +3,14 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../../client/build')));
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../../c9c-client/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../c9c-client/build', 'index.html'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
