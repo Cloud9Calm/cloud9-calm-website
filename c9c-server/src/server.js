0920d0f7fd0b831de3c5ae46ee14880f75431dcd
erincronie <erincronie@gmail.com>
2024-06-12 11:04:06 -0700

    Set up environment variables

--- c9c-server/src/server.js
+++ c9c-server/src/server.js
@@ -1,14 +1,18 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const app = express();
 
+const port = process.env.PORT || 3000;
+
+// Serve static files from the 'c9c-client/public' directory
 app.use(express.static(path.join(__dirname, 'c9c-client', 'public')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'c9c-client', 'public', 'index.html'));
 });
 
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
