7669f3ff1308a170873b60ec717e168e8729e5a6
erincronie <erincronie@gmail.com>
2024-06-12 10:53:52 -0700

    Fix index.html path

--- c9c-server/src/server.js
+++ c9c-server/src/server.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../../c9c-client/build')));
+app.use(express.static(path.join(__dirname, 'c9c-client', 'public')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../c9c-client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'c9c-client', 'public', 'index.html'));
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is up on port ${port}`);
 });
