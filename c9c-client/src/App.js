b5bc416f7eda09b11f9c306acffa17759133cb21
erincronie <erincronie@gmail.com>
2024-08-13 11:36:33 -0700

    updating clients section:

--- c9c-client/src/App.js
+++ c9c-client/src/App.js
@@ -1,10 +1,25 @@
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
+import ReactGA from 'react-ga4';
+import CookieConsent, { getCookieConsentValue } from 'react-cookie-consent';
 import '../src/styles/partials/_global.scss';
-import Home from './pages/Home/Home'; 
+import Home from './pages/Home/Home';
 import WebsiteDevelopment from './pages/WebsiteDevelopment/WebsiteDevelopment';
 import EcommerceSupport from './pages/EcommerceSupport/EcommerceSupport';
 import Footer from './components/Footer/Footer';
+
+// Ensure the measurement ID is defined
+const measurementId = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID;
+if (!measurementId) {
+  throw new Error('REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID environment variable is required');
+}
+
+// Initialize Google Analytics only if consent is given
+useEffect(() => {
+  if (getCookieConsentValue() === "true") {
+    ReactGA.initialize(measurementId);
+  }
+}, []);
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -16,7 +31,18 @@ function ScrollToTop() {
   return null;
 }
 
+function usePageViews() {
+  const location = useLocation();
+  useEffect(() => {
+    if (getCookieConsentValue() === "true") {
+      ReactGA.send({ hitType: "pageview", page: location.pathname + location.search });
+    }
+  }, [location]);
+}
+
 function App() {
+  usePageViews();
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -28,6 +54,20 @@ function App() {
         </Routes>
       </BrowserRouter>
       <Footer />
+      <CookieConsent
+        location="bottom"
+        buttonText="I understand"
+        cookieName="cloud9CalmCookieConsent"
+        style={{ background: "#2B373B" }}
+        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+        expires={150}
+        onAccept={() => {
+          ReactGA.initialize(measurementId);
+        }}
+      >
+        This website uses cookies to ensure you get the best experience on our website. By continuing to use our site, you accept our use of cookies.{" "}
+        <span style={{ fontSize: "10px" }}>Learn more in our <a href="/privacy-policy" style={{ color: "#fff" }}>Privacy Policy</a>.</span>
+      </CookieConsent>
     </div>
   );
 }
